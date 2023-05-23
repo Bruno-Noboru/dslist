3ce3bd338da532ded91719f304d5806cca2f02c0
Bruno Noboru <bntatematsu@gmail.com>
2023-05-23 20:49:20 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,13 +1,13 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
 	private Long id;
 	private String title;
 	private Integer year;
-	private String genre;
 	private String imgUrl;	
 	private String shortDescription;
 	
@@ -18,9 +18,16 @@ public class GameMinDTO {
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
-		genre = entity.getGenre();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
@@ -35,9 +42,7 @@ public class GameMinDTO {
 		return year;
 	}
 
-	public String getGenre() {
-		return genre;
-	}
+	
 
 	public String getImgUrl() {
 		return imgUrl;
